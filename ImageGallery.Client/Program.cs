e00829fa80ba902f75278d12127e8eee12ba6e54
Varungupta <varun.gupta@everi.com>
2024-01-08 16:37:31 +0530

    First Commit

---
+++ ImageGallery.Client/Program.cs
@@ -0,0 +1,58 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.Net.Http.Headers;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews()
+    .AddJsonOptions(configure => 
+        configure.JsonSerializerOptions.PropertyNamingPolicy = null);
+
+// create an HttpClient used for accessing the API
+builder.Services.AddHttpClient("APIClient", client =>
+{
+    client.BaseAddress = new Uri(builder.Configuration["ImageGalleryAPIRoot"]);
+    client.DefaultRequestHeaders.Clear();
+    client.DefaultRequestHeaders.Add(HeaderNames.Accept, "application/json");
+});
+
+builder.Services.AddAuthentication((o =>
+{
+    o.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    o.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+})).AddCookie(CookieAuthenticationDefaults.AuthenticationScheme).AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme,
+    o =>
+    {
+        o.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        o.Authority = "https://localhost:5001";
+        o.ClientId = "imagegalleryclient";
+        o.ClientSecret = "secret";
+        o.ResponseType = "code";
+        //o.Scope.Add("openid");
+        //o.Scope.Add("profile");
+        //o.CallbackPath = new PathString("signin-oidc");
+        o.SaveTokens = true;
+    });
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler();
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Gallery}/{action=Index}/{id?}");
+
+app.Run();
---
+++ Local.IDP/Pages/TestUsers.cs
@@ -0,0 +1,64 @@
+// Copyright (c) Duende Software. All rights reserved.
+// See LICENSE in the project root for license information.
+
+
+using IdentityModel;
+using System.Security.Claims;
+using System.Text.Json;
+using Duende.IdentityServer;
+using Duende.IdentityServer.Test;
+
+namespace Everi.IDP;
+
+public class TestUsers
+{
+    public static List<TestUser> Users
+    {
+        get
+        {
+            var address = new
+            {
+                street_address = "One Hacker Way",
+                locality = "Heidelberg",
+                postal_code = 69118,
+                country = "Germany"
+            };
+
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "3C0BE9B6-C905-47D8-BC0F-83A91A7F338A",
+                    Username = "alice",
+                    Password = "alice",
+                    Claims =new List<Claim>()
+                    {
+                        new Claim(JwtClaimTypes.Name, "Alice Smith"),
+                        new Claim(JwtClaimTypes.GivenName, "Alice"),
+                        new Claim(JwtClaimTypes.FamilyName, "Smith"),
+                        new Claim(JwtClaimTypes.Email, "AliceSmith@email.com"),
+                        new Claim(JwtClaimTypes.EmailVerified, "true", ClaimValueTypes.Boolean),
+                        new Claim(JwtClaimTypes.WebSite, "http://alice.com"),
+                        new Claim(JwtClaimTypes.Address, JsonSerializer.Serialize(address), IdentityServerConstants.ClaimValueTypes.Json)
+                    }
+                },
+                new TestUser
+                {
+                    SubjectId = "8437D07C-5D2E-40CA-9E85-F3C0ADB41DAB",
+                    Username = "bob",
+                    Password = "bob",
+                    Claims = new List<Claim>()
+                    {
+                        new Claim(JwtClaimTypes.Name, "Bob Smith"),
+                        new Claim(JwtClaimTypes.GivenName, "Bob"),
+                        new Claim(JwtClaimTypes.FamilyName, "Smith"),
+                        new Claim(JwtClaimTypes.Email, "BobSmith@email.com"),
+                        new Claim(JwtClaimTypes.EmailVerified, "true", ClaimValueTypes.Boolean),
+                        new Claim(JwtClaimTypes.WebSite, "http://bob.com"),
+                        new Claim(JwtClaimTypes.Address, JsonSerializer.Serialize(address), IdentityServerConstants.ClaimValueTypes.Json)
+                    }
+                }
+            };
+        }
+    }
+}
